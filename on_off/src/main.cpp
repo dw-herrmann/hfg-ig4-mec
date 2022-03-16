4b8309fe8bbe0fa4560a3d846866f6a7d23c14e6
dw-herrmann <mail@dw-herrmann.de>
2022-03-16 14:02:40 +0100

    neuen Pfad hinzufügen

---
+++ on_off/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+int motor = 8;
+int relay = 10;
+int direction = false;
+
+void setup()
+{
+  pinMode(motor, OUTPUT);
+  pinMode(relay, OUTPUT);
+  // test
+}
+
+void loop()
+{
+  // anschalten
+  digitalWrite(motor, HIGH);
+
+  // 3000 ms
+  delay(3000);
+
+  // ausschalten
+  digitalWrite(motor, LOW);
+
+  // 100 ms delay
+  delay(100);
+
+  // direction umschalten und relay ansprechen
+  direction = !direction;
+  digitalWrite(relay, direction);
+}
